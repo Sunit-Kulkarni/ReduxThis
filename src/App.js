5e7aff30fdbcd0399adeb7d784aa8b635ec43188
Sunit Kulkarni <sunit.kulkarni@protonmail.com>
2018-09-20 13:49:49 -0400

    imported store

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { Provider } from 'react-redux';
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
 
+import store from './store';
+
 class App extends Component {
   render() {
     return (
